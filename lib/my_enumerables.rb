35b854b1c6c01f1d7059c60435db7136b77d21fc
amrxt <amrxt11@outlook.com>
2023-02-11 15:37:04 +0530

    Complete

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,59 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    i = 0
+    for x in self
+      yield(x,i)
+      i+=1
+    end
+  end
+
+  def my_select(&block)
+    output_arr = []
+    for x in self
+      output_arr.push(x) if (yield x)
+    end
+    output_arr
+  end
+
+  def my_all?
+    for x in self
+      (return false) unless yield x
+    end
+    true
+  end
+
+  def my_any?
+    for x in self
+      (return true) if yield x
+    end
+    false
+  end
+
+  def my_none?(&passed_block)
+    (!(self.my_any?(&passed_block))) ? (return true) : (return false)
+  end
+
+  def my_count(&passed_block)
+    (return self.length) unless block_given?
+    
+    return (self.my_select(&passed_block)).length
+  end
+
+  def my_map(&passed_block)
+    output_arr = []
+    for x in self
+      output_arr.push(yield x)
+    end
+    output_arr
+  end
+
+  def my_inject(_initial_value = 0)
+    for i in self
+      _initial_value = yield(_initial_value, i)
+    end
+    _initial_value
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +62,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each()
+    for x in self
+      yield x
+    end
+  end
 end
+
